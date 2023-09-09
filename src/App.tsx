5c5762c6d9a765f6045d435024b262304b6d9050
hunderaweke <hunderaweke@gmail.com>
2023-09-09 15:18:54 +0300

    added: Contact and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import AboutMe from "./components/AboutMe";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Portfolio from "./components/Porfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar />
       <Hero />
       <AboutMe />
       <Projects />
       <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 }
