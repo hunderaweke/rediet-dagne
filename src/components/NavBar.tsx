35e93b04d79413a7be5b94775b60575e5b752f5e
hunderaweke <hunderaweke@gmail.com>
2023-09-09 15:20:29 +0300

    changed: navbar to flowbite navbar:

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,32 @@
+import { Navbar } from "flowbite-react";
 function NavBar() {
   return (
     <>
-      <div className="sticky top-1 z-40 w-full backdrop-blur-lg flex-none transition-colors duration-500  supports-backdrop-blur:bg-white/75 dark:bg-[#02141C]/80 flex justify-between">
-        <div className="px-20 py-3">
-          <h1 className="font-brand tracking-wider inline text-transparent bg-clip-text bg-gradient-to-br font-bold from-[#0BE4CC] to-sky-500 lg:text-5xl md:text-4xl">
-            Rediet
+      <Navbar className="flex sticky text-3xl top-0 z-10 justify-between  bg-transparent  backdrop-blur-lg supports-backdrop-blur:bg-white/75 dark:bg-[#02141C]/80 ">
+        <Navbar.Brand href="#">
+          <h1 className="font-brand text-transparent bg-clip-text bg-gradient-to-br from-[#0BE4CC] to-sky-500 text-4xl">
+            Rediet Dagne
           </h1>
-          <h1 className="font-brand ml-4 inline text-transparent bg-clip-text bg-gradient-to-bl font-bold from-[#0BE4CC] to-sky-500 lg:text-5xl md:text-4xl">
-            Dagne
-          </h1>
-          <div className="w-10 h-10 rounded-full t bg-gradient-to-bl font-bold ffrom-[#0BE4CC] to-sky-500 absolute blur-lg left-48 top-3"></div>
-        </div>
-        <div className="px-10 font-normal flex text-xl  items-center gap-10 text-transparent bg-clip-text bg-gradient-to-bl font-bold from-[#0BE4CC] to-sky-500 tracking-[-0.1rem]">
-          <h2>About</h2>
-          <h2>Portfolio</h2>
-          <h2>Projects</h2>
-          <h2>Testimonial</h2>
-          <button className="px-5 py-1 tracking-tighter text-white rounded-full bg-gradient-to-bl from-[#0BE4CC] to-sky-500">
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="font-normal text-center">
+          <Navbar.Link className="navbar-links" href="#">
+            About
+          </Navbar.Link>
+          <Navbar.Link className="navbar-links" href="#">
+            Projects
+          </Navbar.Link>{" "}
+          <Navbar.Link className="navbar-links" href="#">
+            Portfolio
+          </Navbar.Link>{" "}
+          <Navbar.Link className="navbar-links" href="#">
+            Testimonial
+          </Navbar.Link>
+          <Navbar.Link className="navbar-links" href="#">
             Hire Me
-          </button>
-        </div>
-      </div>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 }
