1186bf4a7fafa084e0c96abcbfa94ce26059037c
hunderaweke <hunderaweke@gmail.com>
2023-09-09 15:19:50 +0300

    updated: responsive design patterns

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,11 +3,11 @@ function AboutMe() {
     <>
       <div id="about" className="mt-10">
         <div className="w-full text-center">
-          <h1 className="font-brand text-5xl bg-clip-text bg-gradient-to-r from-[#0BE4CC] to-sky-500 text-transparent">
+          <h1 className="header">
             About Me
           </h1>
         </div>
-        <div className="px-10 lg:px-48 py-12 mt-10">
+        <div className="px-5 text-center md:px-10 lg:px-48 py-3 md:py-12 mt-10">
           <p className="font-normal tracking-tighter">
             I am a passionated graphics designer with high level of skill and
             professional ethics and philosophy with so many projects and
@@ -19,13 +19,6 @@ function AboutMe() {
             atque at libero aperiam ab sequi facilis earum error. Incidunt, quas
             maxime. Porro saepe nam velit fugiat, aperiam dignissimos blanditiis
             optio provident corporis aut ad voluptate vel et culpa laborum est,
-            deleniti nostrum dolor ducimus! Iste nulla quibusdam, ad alias
-            similique ab modi exercitationem. Repellat reprehenderit illum
-            impedit odit, alias totam. Culpa ad neque voluptas natus. Ex et,
-            quisquam numquam, nisi vel error earum aliquid, esse quis rerum
-            aperiam alias corporis officia ratione. Praesentium cumque incidunt
-            asperiores dolore distinctio corrupti nisi temporibus, maxime
-            delectus ab voluptas est exercitationem odio tenetur accusantium.
           </p>
         </div>
       </div>
